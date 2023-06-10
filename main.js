489ce76e10413c68fff41e327bd6d5714374730a
Ajmain Fayek <ajmainfayek@creations.onmicrosoft.com>
2023-06-10 09:11:29 +0600

    js file added

---
+++ main.js
@@ -0,0 +1,13 @@
+let makeSubmitButtonInteractive = document.getElementById('.submit');
+
+function makeSubmitButtonInteractive(button) {
+  // Add a hover effect to the button
+  button.addEventListener("mouseover", function() {
+    this.style.backgroundColor = "lightgray";
+  });
+
+  // Add a click effect to the button
+  button.addEventListener("click", function() {
+    this.style.backgroundColor = "red";
+  });
+}
